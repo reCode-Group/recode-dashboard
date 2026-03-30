e799da84a81b1993c264878936ec778a502b78dc
true_lnz <kazakkulov.uf@recode-group.ru>
2026-03-31 00:45:28 +0500

    Добавил новую страницу Тариф

--- src/routes.js
+++ src/routes.js
@@ -1,97 +1,89 @@
 // import
-import SignIn from "views/Auth/SignIn.js";
-import SignUp from "views/Auth/SignUp.js";
-import Billing from "views/Dashboard/Billing";
-import Dashboard from "views/Dashboard/Dashboard";
-import Profile from "views/Dashboard/Profile";
-import Support from "views/Dashboard/Support";
-import EmployeeTable from "views/Dashboard/Tables/components/EmployeeTable";
-import ConversionHistoryPage from "views/Dashboard/ConversionHistory";
+import SignIn from 'views/Auth/SignIn.js';
+import ConversionHistoryPage from 'views/Dashboard/ConversionHistory';
+import Dashboard from 'views/Dashboard/Dashboard';
+import Profile from 'views/Dashboard/Profile';
+import Support from 'views/Dashboard/Support';
+import EmployeeTable from 'views/Dashboard/Tables/components/EmployeeTable';
 
-import {
-	CreditIcon,
-	HomeIcon,
-	PersonIcon,
-	RocketIcon,
-	SupportIcon
-} from "components/Icons/Icons";
+import { CreditIcon, HomeIcon, PersonIcon, RocketIcon, SupportIcon } from 'components/Icons/Icons';
 
-import { CompanyIcon, ConverterIcon, EmployersIcon, HistoryIcon } from "components/Icons/Icons";
+import { CompanyIcon, ConverterIcon, EmployersIcon, HistoryIcon } from 'components/Icons/Icons';
+import Billing from 'views/Dashboard/Billing';
+import Tariff from 'views/Dashboard/Tariff';
 
 var dashRoutes = [
-  {
-    path: "/dashboard",
-    name: "Главная",
-    icon: <HomeIcon color="inherit" />,
-    component: Dashboard,
-    layout: "/admin",
-  },
-  {
-    path: "/profile",
-    name: "Профиль",
-    icon: <PersonIcon color="inherit" />,
-    secondaryNavbar: true,
-    component: Profile,
-    layout: "/admin",
-  },
 	{
-    path: "/billing",
-    name: "Финансы",
-    icon: <CreditIcon color="inherit" />,
-    component: Billing,
-    layout: "/admin",
-  },
-  {
-    path: "/support",
-    name: "Техподдержка",
-    icon: <SupportIcon color="inherit" />,
-    component: Support,
-    layout: "/admin",
-    hiddenInSidebar: true,
-  },
+		path: '/dashboard',
+		name: 'Главная',
+		icon: <HomeIcon color="inherit" />,
+		component: Dashboard,
+		layout: '/admin',
+	},
 	{
-    path: "/signup",
-    name: "Тариф",
-    icon: <RocketIcon color="inherit" />,
-    secondaryNavbar: true,
-    component: SignUp,
-    layout: "/auth",
-  },
-  {
-    name: "УПРАВЛЕНИЕ",
-    category: "account",
-    state: "pageCollapse",
-    views: [
+		path: '/profile',
+		name: 'Профиль',
+		icon: <PersonIcon color="inherit" />,
+		secondaryNavbar: true,
+		component: Profile,
+		layout: '/admin',
+	},
+	{
+		path: '/billing',
+		name: 'Финансы',
+		icon: <CreditIcon color="inherit" />,
+		component: Billing,
+		layout: '/admin',
+	},
+	{
+		path: '/support',
+		name: 'Техподдержка',
+		icon: <SupportIcon color="inherit" />,
+		component: Support,
+		layout: '/admin',
+		hiddenInSidebar: true,
+	},
+	{
+		path: '/tariff',
+		name: 'Тариф',
+		icon: <RocketIcon color="inherit" />,
+		component: Tariff,
+		layout: '/admin',
+	},
+	{
+		name: 'УПРАВЛЕНИЕ',
+		category: 'account',
+		state: 'pageCollapse',
+		views: [
 			{
-				path: "/tables",
-				name: "Компания",
+				path: '/tables',
+				name: 'Компания',
 				icon: <CompanyIcon color="inherit" />,
 				component: EmployeeTable,
-				layout: "/admin",
+				layout: '/admin',
 			},
 			{
-				path: "/tables",
-				name: "Сотрудники",
+				path: '/tables',
+				name: 'Сотрудники',
 				icon: <EmployersIcon color="inherit" />,
 				component: EmployeeTable,
-				layout: "/admin",
+				layout: '/admin',
 			},
 			{
-				path: "/login-page",
-				name: "Перейти в переводчик",
+				path: '/login-page',
+				name: 'Перейти в переводчик',
 				icon: <ConverterIcon color="inherit" />,
 				component: SignIn,
-				layout: "/auth",
+				layout: '/auth',
 			},
-						{
-				path: "/conversion-history",
-				name: "История конвертаций",
+			{
+				path: '/conversion-history',
+				name: 'История конвертаций',
 				icon: <HistoryIcon color="inherit" />,
 				component: ConversionHistoryPage,
-				layout: "/admin",
+				layout: '/admin',
 			},
-
-    ],
-  },
+		],
+	},
 ];
 export default dashRoutes;
---
+++ src/views/Dashboard/Tariff/index.js
@@ -0,0 +1,51 @@
+import { Box, Flex, Grid, GridItem } from '@chakra-ui/react';
+import BackgroundCard1 from 'assets/img/BackgroundCard1.png';
+import OtherTariffs from 'views/Dashboard/Billing/components/OtherTariffs';
+import PaymentMethod from 'views/Dashboard/Billing/components/PaymentMethod';
+import TariffCard from 'views/Dashboard/Billing/components/TariffCard';
+
+function Tariff() {
+	return (
+		<Flex direction="column" pt={{ base: '120px', md: '75px' }}>
+			<Box w={{ base: '100%', lg: '70%' }}>
+				<Grid
+					templateColumns={{ base: '1fr', lg: '1fr 1fr' }}
+					templateRows={{ base: 'auto auto auto', lg: 'auto auto' }}
+					templateAreas={{
+						base: `"tariff" "others" "payment"`,
+						lg: `"tariff others" "payment payment"`,
+					}}
+					gap="24px"
+				>
+					<GridItem area="tariff">
+						<TariffCard
+							backgroundImage={BackgroundCard1}
+							title={'Ваш тариф'}
+							tariffName={'БАЗОВЫЙ'}
+							validUntil={{
+								name: 'ДЕЙСТВИТЕЛЕН ДО',
+								data: '25.11.2025',
+							}}
+							tokenBalance={{
+								name: 'ОСТАТОК ТОКЕНОВ',
+								code: '1 100 / 10 000',
+							}}
+							monthlyCost={{
+								name: 'СТОИМОСТЬ',
+								code: '20 000 ₽/мес.',
+							}}
+						/>
+					</GridItem>
+					<GridItem area="others">
+						<OtherTariffs />
+					</GridItem>
+					<GridItem area="payment">
+						<PaymentMethod title={'Способ оплаты'} />
+					</GridItem>
+				</Grid>
+			</Box>
+		</Flex>
+	);
+}
+
+export default Tariff;
